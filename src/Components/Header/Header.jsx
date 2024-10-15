13a0ac75c1ceec13fed88cab62a525198e1115a7
Samir Jabre <samirjabre8@gmail.com>
2024-10-15 21:16:23 +0300

    Modifying the Responsiveness of the Header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import {useState,useEffect} from 'react'
 import './Header.css'
 import logo from '../../assets/logo.png'
 import Navbar from '../../Components/Navbar/Navbar'
 import ContentCircle from '../../base/Content_Circle/ContentCircle'
 
 function Header() {
-  return (
+
+  const [width, setWidth] = useState(window.innerWidth);
+  console.log(width);
+  useEffect(()=>{
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+  },[])
+  
+  return width > 1200 ? (
     <header className='Home_Header'>
             <div className='Home_Logo'>
                 <img src={logo} alt='logo' id='logo'/>
@@ -18,8 +26,14 @@ function Header() {
               <ContentCircle color={'black'} logo={'cart'}/>
               <ContentCircle color={'gray'} logo={'profile'}/>
             </div>
-        </header>
+    </header>
+  ) :
+  (
+    <header>
+      <h1>Hello World</h1>
+    </header>
   )
+
 }
 
 export default Header
