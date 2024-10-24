735af4217fe333ad16aebfb62f3f4e6f7c4364eb
Samir Jabre <samirjabre8@gmail.com>
2024-10-24 13:07:13 +0300

    Footer Finished

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import './Footer.css'
-import logo from '../../Assets/logo.png'
+import logo_white from '../../Assets/logo_white.png'
+import facebook from '../../Assets/icons/facebook.png'
+import instagram from '../../Assets/icons/instagram.png'
+import x from '../../Assets/icons/x.png'
+import youtube from '../../Assets/icons/youtube.png'
 
 function Footer() {
   return (
     <div className='footer_container'>
 
       <div className="footer_top">
-        <div><img id='footer_logo' src={logo} alt="Website Logo" /></div>
+        <div><img id='footer_logo' src={logo_white} alt="Website Logo" /></div>
 
         <div>
           <ul>
@@ -37,7 +41,14 @@ function Footer() {
       <div className="footer_middle"></div>
 
       <div className="footer_bottom">
-        <div className="footer_logos"></div>
+
+        <div className="footer_logos">
+          <img src={facebook} alt="Facebook Icon" />
+          <img src={instagram} alt="Instagram Icon" />
+          <img src={x} alt="X Icon" />
+          <img src={youtube} alt="Youtube Icon" />
+        </div>
+
         <div className="copywrite_text">
           <p>&copy;2024 Copywrite, All rights reserved.</p>
         </div>
