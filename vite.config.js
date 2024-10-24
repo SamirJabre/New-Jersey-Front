3ce71ef3c1fb500eefb720ea1b398fef4546d83a
Samir Jabre <samirjabre8@gmail.com>
2024-10-24 14:10:25 +0300

    Fixing Bugs and Modifying the responsiveness

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: '192.168.252.187', // Allow access from external devices
-    port: 3000, // You can specify any port here
+    host: '192.168.252.187',
+    port: 3000,
   },
   plugins: [react()],
 })
