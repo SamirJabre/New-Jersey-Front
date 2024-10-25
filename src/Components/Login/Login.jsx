3edd6034efc9ec967b55fbfaee58904687716673
Samir Jabre <samirjabre8@gmail.com>
2024-10-26 01:16:46 +0300

    Login Form Finished

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './Login.css'
 import Name from '../../base/Names/Name'
 import Input from '../../base/FormInput/Input'
+import google from '../../assets/icons/google_logo.png'
+import apple from '../../assets/icons/apple_logo.png'
 
 function Login() {
   return (
@@ -21,6 +23,32 @@ function Login() {
                     <Input type={'email'} placeholder={'Enter Email'} name={'Email'}/>
                     <Input type={'password'} placeholder={'Enter Password'} name={'Password'}/>
                 </form>
+                
+                <div className="terms_conditions">
+                    <input type="checkbox" id='terms_checkbox'/>
+                    <p>I agree to the terms and conditions</p>
+                </div>
+                <button type="submit" form='login_form' id='create_account_btn'>Create Account</button>
+                <div className="other_methods">
+                    <div></div>
+                    <p>Or register with</p>
+                    <div></div>
+                </div>
+                <div className="social_media">
+                    <div className="google">
+                        <div className="google_logo">
+                            <img src={google} alt="Google Logo" />
+                        </div>
+                        <p>Google</p>
+                    </div>
+
+                    <div className="apple">
+                    <div className="apple_logo">
+                            <img src={apple} alt="Apple Logo" />
+                        </div>
+                        <p>Apple</p>
+                    </div>
+                </div>
             </div>
             
         </div>
