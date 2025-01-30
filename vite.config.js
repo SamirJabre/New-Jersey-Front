7fd5ebe374a2d50072b8c335ae8c2fe093aa3449
Samir Jabre <samirjabre8@gmail.com>
2025-01-30 17:39:58 +0200

    Update Vite to version 5.4.14 and configure server settings

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // server: {
-  //   host: '192.168.252.187',
-  //   port: 3000,
-  // },
+  server: {
+    host: true,
+    port: 3000,
+  },
   plugins: [react()],
 })
 
