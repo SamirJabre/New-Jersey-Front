5bbf9e4b0894a15af1c52dc927f462fdd22015fc
Samir Jabre <samirjabre8@gmail.com>
2025-02-05 14:45:38 +0200

    Fixing Bugs

--- src/Components/Filter/Filter.jsx
+++ src/Components/Filter/Filter.jsx
@@ -1,50 +1,54 @@
-import React, { useEffect, useState } from "react";
-import dropdown from "../../assets/icons/dropdown.svg";
-import "./Filter.css";
+  import React, { useEffect, useState } from "react";
+  import dropdown from "../../assets/icons/dropdown.svg";
 
-function Filter() {
-  const [showDropdown, setShowDropdown] = useState(false);
+  function Filter() {
+    const [showDropdown, setShowDropdown] = useState(false);
+    const [price, setPrice] = useState(50);
 
-  return (
-    <div className="w-5/6 h-12 m-5 rounded-2xl flex justify-start items-center border border-[#b1b1b1] px-2">
-      <select className="h-full w-14 px-2 bg-transparent">
-        <option defaultChecked={true}>Size</option>
-        <option>XS</option>
-        <option>SM</option>
-        <option>MD</option>
-        <option>LG</option>
-        <option>XL</option>
-        <option>2XL</option>
-      </select>
+    return (
+      <div className="w-[90%] h-10 rounded-xl flex justify-between items-center border border-[#b1b1b1] px-2 font-inter text-sm">
+        <select className="h-full w-fit select-none outline-none bg-transparent">
+          <option defaultChecked={true}>Size</option>
+          <option>XS</option>
+          <option>SM</option>
+          <option>MD</option>
+          <option>LG</option>
+          <option>XL</option>
+          <option>2XL</option>
+        </select>
 
-      <div
-        className="h-full w-fit flex justify-center items-center"
-        typeof="button"
-        onClick={() => setShowDropdown(!showDropdown)}
-      >
-        <label>Price</label>
-        <img src={dropdown} className="scale-75" />
+        <div className="relative h-full outline-none select-none flex flex-col justify-between items-center">
+          <div
+            className="h-full w-fit flex justify-center items-center"
+            typeof="button"
+            onClick={() => setShowDropdown(!showDropdown)}
+          >
+            <label>Price</label>
+            <img src={dropdown} className="scale-75" />
+          </div>
+
+          {showDropdown && (
+            <div className="absolute top-[105%] w-fit h-14 border rounded-2xl border-[#b1b1b1] flex px-2 justify-center items-center">
+              <label>0$</label>
+              <input type="range" defaultValue={50} onChange={(e)=>setPrice(e.target.value)} className="mx-2 "/>
+              <label>{price}$</label>
+            </div>
+          )}
+        </div>
+
+        <select className="h-full w-fit bg-transparent outline-none select-none">
+          <option defaultChecked={true}>Sale</option>
+          <option>50%</option>
+          <option>75%</option>
+        </select>
+
+        <select className="h-full w-fit bg-transparent outline-none select-none">
+          <option defaultChecked={true}>Sort By</option>
+          <option>Asc</option>
+          <option>Desc</option>
+        </select>
       </div>
+    );
+  }
 
-      <select className="h-full w-14 px-2 bg-red-500">
-        <option defaultChecked={true}>Sale</option>
-        <option>50% Sale</option>
-        <option>75% Sale</option>
-      </select>
-
-
-      <select className="h-full w-fit px-2 bg-transparent">
-        <option defaultChecked={true}>Product Type</option>
-        <option>Men</option>
-        <option>Women</option>
-        <option>Adidas</option>
-        <option>Nike</option>
-        <option>Reebok</option>
-        <option>Puma</option>
-      </select>
-
-    </div>
-  );
-}
-
-export default Filter;
+  export default Filter;
