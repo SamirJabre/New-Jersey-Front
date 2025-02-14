0a44d4816abf7f132f7daea125cce1c5a3541df7
Samir Jabre <samirjabre8@gmail.com>
2025-02-14 16:28:30 +0200

    Refactor Footer component for improved readability and structure

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -11,21 +11,58 @@ function Footer() {
     <footer className="w-full h-fit bg-[#101010] flex flex-col items-center justify-between py-5">
       <div className="w-full h-60 flex flex-col justify-between items-center">
         <div className=" w-full h-[40%]">
-          <img src={logo_white} alt="Website Logo" className="w-full h-full object-contain"/>
+          <img
+            src={logo_white}
+            alt="Website Logo"
+            className="w-full h-full object-contain"
+          />
         </div>
-        <p className="text-white font-inter text-xs text-center">Lebanon, Tripoli, Al-Mina, Next To OMT, 961 3 330 550</p>
-        <a href="#" className="text-blue-600 text-xs font-inter">Call us at +961 81 165 027</a>
+        <p className="text-white font-inter text-xs text-center">
+          Lebanon, Tripoli, Al-Mina, Next To OMT, 961 3 330 550
+        </p>
+        <a href="#" className="text-blue-600 text-xs font-inter">
+          Call us at +961 81 165 027
+        </a>
         <div className="flex justify-between w-2/3 h-fit items-center">
-          <a href="#"><img src={facebook} alt="" className="scale-75"/></a>
-          <a href="#"><img src={instagram} alt="" className="scale-75"/></a>
-          <a href="#"><img src={x} alt="" className="scale-75"/></a>
-          <a href="#"><img src={youtube} alt="" className="scale-75"/></a>
+          <a href="#">
+            <img src={facebook} alt="" className="scale-75" />
+          </a>
+          <a href="#">
+            <img src={instagram} alt="" className="scale-75" />
+          </a>
+          <a href="#">
+            <img src={x} alt="" className="scale-75" />
+          </a>
+          <a href="#">
+            <img src={youtube} alt="" className="scale-75" />
+          </a>
         </div>
       </div>
-      <hr className="w-full h-1 m-5" style={{borderColor:'gray'}}/>
+      <hr className="w-full h-1 m-5" style={{ borderColor: "gray" }} />
 
-
-      
+      <div className="h-fit w-full px-5 flex flex-wrap justify-between items-center">
+        <div className="w-1/3 h-20 mb-8">
+          <ul className="font-inter text-sm font-light text-gray-300 flex flex-col items-start justify-between h-full">
+            <li>Weekly Themes</li>
+            <li>Pre-Sale</li>
+            <li>Submit A Ticket</li>
+          </ul>
+        </div>
+        <div className="w-1/3 h-20  mb-8">
+          <ul className="font-inter text-sm font-light text-gray-300 flex flex-col items-start justify-between h-full">
+            <li>Services</li>
+            <li>Find Us</li>
+            <li>About</li>
+          </ul>
+        </div>
+        <div className="w-1/2 h-20  mb-8">
+          <ul className="font-inter text-sm font-light text-gray-300 flex flex-col items-start justify-between h-full">
+            <li>Support</li>
+            <li>Contact Us</li>
+            <li>Chupapi Munianioo</li>
+          </ul>
+        </div>
+      </div>
     </footer>
   );
 }
